84f7472aefa4ce4412f58659b685cd249ff09745
s7eve <techs7eve@gmail.com>
2023-06-05 23:11:27 +0800

    refactor: unused css reset, attribution component
    - remove css reset on main.css since tailwind already has 'preflight' which serve the same purpose
    - add full width by default for attribution component

--- src/components/AttributionComp.vue
+++ src/components/AttributionComp.vue
@@ -12,7 +12,7 @@ defineProps({
 </script>
 
 <template>
-	<div class="attribution">
+	<div class="attribution w-full">
 		Challenge by
 		<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
 		Coded by <a :href="attributorLink" target="_blank">{{ attributorName }}</a
